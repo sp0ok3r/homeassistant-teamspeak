e034a06fca1ede1b5969feb1307688d328fc1ade
sp0ok3r <digitalfoxanon@hotmail.com>
2025-06-12 13:11:09 +0100

    Update __init__.py
    Added support for last home assistant update 12-6-25

--- custom_components/teamspeak/__init__.py
+++ custom_components/teamspeak/__init__.py
@@ -54,12 +54,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
+    to_setup = []
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
+            to_setup.append(platform)
+
+    await hass.config_entries.async_forward_entry_setups(entry, to_setup)
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
